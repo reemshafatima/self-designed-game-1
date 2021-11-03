3efbe20795dac654f31c8bc7b5bf5860c2774d36
reemshafatima <88037019+reemshafatima@users.noreply.github.com>
2021-11-03 23:07:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var dragon,dragonImg
+var fireball,fireballImg
+var flag,flagImg
+var gameOver,gameOverImg
+var spike,spikeImg
+var prince,princeImg
+var princess,princessImg
+var rockwall,rockwallImg
+var startarrow,startarrowImg
+var youwin,youwinImg
+var castle,castleImg
+
+function  preload() {
+dragonImg=loadImage("dragon.png")
+fireballImg=loadImage("frieball.png")
+flagImg=loadImage("flag.png")
+gameOverImg=loadImage("gameOver.png")
+spikeImg=loadImage("spike.png")
+princeImg=loadImage("prince.png")
+princessImg=loadImage("princess.png")
+rockwallImg=loadImage("rockwall.png")
+startarrowImg=loadImage("startarrow.png")
+youwinImg=loadImage("youwin.png")
+castleImg=loadImage("castle.png")
+}
+
+
+
+
+
+
+
